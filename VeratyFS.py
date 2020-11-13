8a4c7e1fdd905cee2a7450f903fe5858e397550b
Jardel G. Marceno <jmarceno@gmail.com>
2020-11-13 11:18:50 -0300

    Removed Deprecaed Function Removed datatype conversion at beggining of write function

--- VeratyFS.py
+++ VeratyFS.py
@@ -702,7 +702,7 @@ class Operations(pyfuse3.Operations):
         return True
 
     async def write(self, fh, offset, buf):                
-        buf = memoryview(buf)
+        # buf = memoryview(buf)
         
         f = self.inodes[fh]
         
@@ -773,17 +773,6 @@ VERATYFS OPERATIONS AND NON OS DEPENDENT CODE
 DEDUP, COMPRESSION, WRITE, DELETE, INDEXES MAINTAINANCE
 
 '''
-
-def get_small_blocks_real_size(start_path = '.'):
-    total_size = 0
-    for dirpath, dirnames, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            # skip if it is symbolic link
-            if not os.path.islink(fp):
-                total_size += os.path.getsize(fp)
-
-    return total_size
 
 def get_usage(stat_msg_queue):
     """
