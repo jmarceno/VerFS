3e9040b36aa249ad9f81bb767540965c0df5990f
Jardel G. Marceno <jmarceno@gmail.com>
2020-11-28 12:46:45 -0300

    Added PyTest as a requirement Improved test.sh to display total tim per operation

--- tests/test.sh
+++ tests/test.sh
@@ -5,11 +5,16 @@ echo
 echo
 echo De temp para VeratyFS
 echo HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-rsync -r --progress /run/media/jmarceno/01D618D8C54EB2C0/temp/* ~/fsteste/
+time rsync -r --progress /run/media/jmarceno/01D618D8C54EB2C0/temp/* ~/fsteste/
+fusermont -u ~/fsteste
+cd ~/metadata
+rm -r *
 
 echo 
 echo
 echo
 echo De temp para SSD ext4
 echo HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
-rsync -r --progress /run/media/jmarceno/01D618D8C54EB2C0/temp/* ~/metadata/
+time rsync -r --progress /run/media/jmarceno/01D618D8C54EB2C0/temp/* ~/tmp/
+cd ~/tmp
+rm -r *
